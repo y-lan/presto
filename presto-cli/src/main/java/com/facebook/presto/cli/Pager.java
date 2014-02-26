034ee785905ad8a62cb546ea90d55202d97f8bf3
yuyang-lan <yuyang.lan@gree.net>
2014-02-26 10:30:24 +0900

    change to not depend on apache.commons.lang3 for os check

--- presto-cli/src/main/java/com/facebook/presto/cli/Pager.java
+++ presto-cli/src/main/java/com/facebook/presto/cli/Pager.java
@@ -14,7 +14,6 @@
 package com.facebook.presto.cli;
 
 import com.google.common.collect.ImmutableList;
-import org.apache.commons.lang3.SystemUtils;
 
 import javax.annotation.Nullable;
 
@@ -121,7 +120,7 @@ public class Pager
 
     public static Pager create(List<String> command)
     {
-        if (SystemUtils.IS_OS_WINDOWS) {
+        if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
             return createNullPager();
         }
 
